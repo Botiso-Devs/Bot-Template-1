8ee0d9397e79259b1593a34d3ce9ef0fefcdb6f3
Botiso-Devs <44485369+Botiso-Devs@users.noreply.github.com>
2018-10-29 15:29:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const Discord = require("discord.js")
 const bot = new Discord.Client()
 
+bot.on("ready", () => {
+  console.log("Bot Online")
+  
+ bot.user.setActivity("Claim This Bot Today", {type: "STREAMING'})
+});
+
+bot.on("message", (message) => {
+  if(message.content === "+test") {
+    message.channel.send("Ok")
+    }
+  });
+  
 bot.login(process.env.TOKEN)
